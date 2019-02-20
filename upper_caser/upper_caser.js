43bad1403e1af4b40383d955881bf3aaef1807a6
ghr85 <ghrenfrew@gmail.com>
2019-02-20 18:52:21 +0000

    upercaser functionality added to uppercaser model

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -2,10 +2,11 @@ const UpperCaser = function (words) {
   this.words = words;
 }
 
-UpperCaser.prototype.toUpperCase = function () {
-return this.words.map(function(el){
-  return el.toUpperCase
-}))
-}
+UpperCaser.prototype.toUpperCase = function(){
+return this.words.map(
+  el => el.toUpperCase()
+)
+};
+
 
 module.exports = UpperCaser;
