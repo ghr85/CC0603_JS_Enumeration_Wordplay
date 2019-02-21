58798ee2b4ee8bc4c258b55e82db49404f89390d
ghr85 <ghrenfrew@gmail.com>
2019-02-21 07:37:22 +0000

    got PangramFinder working

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,28 @@
 const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.toUpperCase().split('');
+  this.phrase = phrase.toUpperCase().split('');
 }
+
+
+PangramFinder.prototype.isEqual = function (ary, duplicates) {
+
+if(ary.length == duplicates.length){
+  return true
+} else {
+  return false
+};
+};
 
 PangramFinder.prototype.isPangram = function () {
 
-}
+  let clone = this.alphabet.reduce(
+    (accumulator,current) => {
+      if (this.phrase.includes(current) === true){
+      accumulator.push(current);}
+      return accumulator;
+    },[]
+  );
+  return this.isEqual(this.alphabet, clone)
+};
 
 module.exports = PangramFinder;
