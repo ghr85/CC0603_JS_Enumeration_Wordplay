09f17e5024364753358221bd0d991bbfec1b7034
ghr85 <ghrenfrew@gmail.com>
2019-02-21 07:02:13 +0000

    added functionality to isogram finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,26 @@
 const IsogramFinder = function (word) {
-
+this.word = word.split('');
 }
+
+IsogramFinder.prototype.isEmpty = function (ary, duplicates) {
+if(ary.length == duplicates.length){
+  return true
+} else {
+  return false
+};
+};
 
 IsogramFinder.prototype.isIsogram = function () {
+   let duplicates = this.word.reduce(
+    (accumulator,current) => {
+      if (accumulator.includes(current) === false){
+      accumulator.push(current);}
+      return accumulator;
+    },[]
+  );
+  return this.isEmpty(this.word,duplicates)
+};
 
-}
+
 
 module.exports = IsogramFinder;
